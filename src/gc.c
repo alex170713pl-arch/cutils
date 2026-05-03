58cc688ac73de29a7ccc8b36d9e4b18f7fe4a2b5
Your Name <you@example.com>
2026-05-03 21:00:42 +0300

    c

--- src/gc.c
+++ src/gc.c
@@ -1,5 +1,6 @@
 #include "../include/gc.h"
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -8,20 +9,41 @@ typedef struct {
     int lifetime;
     int pinned;
 } GCObject;
+typedef struct _Block{
+    GCObject* obj;
+    size_t max;
+    size_t in;
+    struct _Block* next;
+} GCBlock;
 typedef struct {
-    GCObject* tabl;
-    size_t tabs;
-    size_t tabln;
+    GCBlock* header;
     const char* name;
 } GCTable;
 typedef struct {
-    GCTable* Tabs;
+    GCTable** Tabs;
     size_t c;
     size_t l;
 } GCTables;
+
+typedef struct {
+    char* name;
+    GCObject* o;
+} Import_GCObject;
+typedef struct _Ibk{
+    struct _Ibk* next;
+    Import_GCObject* o;
+    size_t in;
+    size_t cap;
+} Import_GCBlock;
+typedef struct {
+    Import_GCBlock* header;
+} Import_Table;
+
+Import_Table tab;
 GCTables tables;
-size_t currt = (size_t)-1;
-size_t gtable = (size_t)-1;
+GCTable* curr;
+GCTable* gtable;
+
 char* __strdup(const char* input) {
     if (!input) return NULL;
     size_t i = strlen(input) + 1;
@@ -30,129 +52,23 @@ char* __strdup(const char* input) {
     memcpy(news,input,i);
     return news;
 }
-size_t _get_tab(void) {
+GCTable* _get_table(void) {
     if (!tables.Tabs) {
-        tables.Tabs = calloc(12, sizeof(GCTable));
-        if (!tables.Tabs) return (size_t)-1;
-        tables.c = 12;
+        tables.Tabs = calloc(12,sizeof(GCTable*));
+        if (!tables.Tabs) return  NULL;
         tables.l = 0;
+        tables.c = 12;
     }
+    GCTable* new = calloc(1,sizeof(GCTable));
+    if (!new) return  NULL;
     if (tables.c == tables.l) {
         size_t newc = tables.c * 2;
-        GCTable* new_tabs = realloc(tables.Tabs, newc * sizeof(GCTable));
-        if (!new_tabs) return (size_t)-1;
-        tables.Tabs = new_tabs;
+        GCTable** newb = realloc(tables.Tabs,newc * sizeof(GCTable*));
+        if (!newb) return NULL;
+        tables.Tabs = newb;
         tables.c = newc;
     }
-    return tables.l++; // индекс
-}
-
-GCObject* _get_obj(GCTable* t) {
-    if (!t) return NULL;
-    if (!t->tabl) {
-        t->tabl = calloc(12, sizeof(GCObject));
-        if (!t->tabl) return NULL;
-        t->tabs = 12;
-        t->tabln = 0;
-    }
-    if (t->tabs == t->tabln) {
-        size_t newl = t->tabs * 2;
-        GCObject* objs = realloc(t->tabl, newl * sizeof(GCObject));
-        if (!objs) return NULL;
-        t->tabl = objs;
-        t->tabs = newl;
-    }
-    return &t->tabl[t->tabln++];
-}
-size_t _find_p(GCTable* t , void* p) {
-    if (!t || !p) return (size_t)-1;
-    size_t i;
-    for (i = 0 ; i < t->tabln;i++)
-        if (t->tabl[i].data == p)
-            return i;
-    return (size_t)-1;
-}
-void gc_select(void* p, ...) {
-    if (gtable == (size_t)-1) {
-        gtable = _get_tab();
-        if (gtable == (size_t)-1) return;
-    }
-    if (currt == (size_t)-1) {
-        currt = _get_tab();
-        if (currt == (size_t)-1) return;
-    }
-
-    GCTable* ct = &tables.Tabs[currt];
-
-    va_list list;
-    va_start(list, p);
-    void* arg = p;
-    while (arg) {
-        GCObject* o = _get_obj(ct);
-        if (!o) { va_end(list); return; }
-        o->data = arg;
-        o->lifetime = 1;
-        o->pinned = 0;
-        arg = va_arg(list, void*);
-    }
-    va_end(list);
-}
-void gc_set_life_time(void* selected,int time) {
-    if (time <= 0) return;
-    GCTable* T = &tables.Tabs[currt];
-    size_t i = _find_p(T,selected);
-    if (i == (size_t)-1) return;
-    T->tabl[i].lifetime = time;
-}
-void gc_lock(void* p) {
-    GCTable* T = &tables.Tabs[currt];
-    size_t i = _find_p(T,p);
-    if (i == (size_t)-1) return;
-    T->tabl[i].pinned = 1;
-}
-void gc_unlock(void* p) {
-    GCTable* T = &tables.Tabs[currt];
-    size_t i = _find_p(T,p);
-    if (i == (size_t)-1) return;
-    T->tabl[i].pinned = 0;
-}
-void gc_collect(int collections_count) {
-    if (collections_count <= 0) return;
-
-    GCTable* T = &tables.Tabs[currt];
-    size_t i;
-    for ( i = 0; i < T->tabln; ) {
-        GCObject* c = &T->tabl[i];
-        if (!c->pinned) {
-            c->lifetime -= collections_count;
-            if (c->lifetime <= 0) {
-                free(c->data);
-                T->tabln--;
-                if (i < T->tabln)
-                    T->tabl[i] = T->tabl[T->tabln];
-                continue;
-            }
-        }
-        i++;
-    }
-}
-void gc_table(const char* table_name) {
-    if (!table_name) return;
-    size_t i = _get_tab();
-    if (i == (size_t)-1) return;
-    GCTable* T = &tables.Tabs[i];
-    if (T->name)
-        free(T->name);
-    T->name = __strdup(table_name);
-}
-void gc_setTable(const char* table_name) {
-    if (!table_name) return;
-    size_t i;
-    for (i = 0;i < tables.l;i++) {
-        if (tables.Tabs[i].name != NULL &&
-            strcmp(tables.Tabs[i].name,table_name) == 0) {
-                currt = i;
-                break;
-            }
-    }
+    tables.Tabs[tables.l] = new;
+    tables.l++;
+    return  new;
 }
